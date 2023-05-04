19fe649de8eac7c2c97736dd2edceb8cd5288dbc
Leon <leon.meyer.zum.alten.borgloh@urs-os.de>
2023-05-04 14:46:10 +0200

    04.05 neues

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,22 @@
+<article class="flex bg-blue-100 border border-gray-800 p-6 rounded-xl space-x-4">
+                        <div class="flex-shrink-0">
+                            <img src="https://i.pravatar.cc/100" alt="" width="100" height="100" class="rounded-xl">
+                        </div>
+
+                        
+                        <div>
+                            <header class="mb-4">
+                                <h3 class="front-bold"> John Doe   </h3>
+                                <p class="text-xs">Posted 
+                                    <time> 800 months ago </time>
+                                </p>
+                            </header>
+
+                            <p>
+                                das hier ist ein wirklich langer text mhm.
+                                sehr lang.
+                                jo
+                            </p>
+
+                        </div>
+                    </article>
